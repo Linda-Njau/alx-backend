0afddaf95b989a5545f2635ab40633a72e242989
Linda-Njau <lindanjau21@gmail.com>
2023-08-02 11:52:48 +0300

    task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -26,7 +26,8 @@ def index():
 
 @babel.localeselector
 def get_locale():
-    """
-    Matches the language from the Accept-Language header
-    """
+    """Returns best available language"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

1b1365b5f4f867cf6b8274325618c60dd677c45c
Linda-Njau <lindanjau21@gmail.com>
2023-08-02 11:29:15 +0300

    task 1 route correction

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -9,15 +9,18 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config(object):
     """Config for babel"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
-    
+
+
 app.config.from_object('1-app.Config')
-    
-@app.route('/')
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """Returns the index page"""
     render_template('1-index.html')

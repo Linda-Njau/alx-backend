a4a3728d2e0d2a20c23ec745aa0dd0898ae2dd6a
Linda-Njau <lindanjau21@gmail.com>
2023-08-02 11:39:17 +0300

    pycodestyle

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -7,18 +7,22 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config(object):
     """Config class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
-    
+
+
 app.config.from_object('2-app.Config')
+
 
 @app.route("/")
 def index():
     """Returns index page"""
     return render_template('2-index.html')
+
 
 @babel.localeselector
 def get_locale():

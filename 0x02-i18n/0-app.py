b4e75cb7bec3a07f37661780a16faa2951aaa2cd
Linda-Njau <lindanjau21@gmail.com>
2023-08-02 11:20:43 +0300

    documenting task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python3
+"""Flask application"""
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """Returns index page"""
     return render_template('index.html')
